d019540449e0527c862c5ad7f2daa78a893caeb0
iRuize <cnruize0503@gmail.com>
2025-07-29 11:03:51 +0800

    仓库初始化

---
+++ src/test/java/ruize/mindmate_backend/MindMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ruize.mindmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
