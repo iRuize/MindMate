3e2e660b2b3490ed44452a0c4b3db4645419acca
iRuize <cnruize0503@gmail.com>
2025-07-29 16:30:32 +0800

    认证模块创建完成

---
+++ MindMate_backend/mindmate-auth/src/main/java/ruize/mindmateauth/MindmateAuthApplication.java
@@ -0,0 +1,13 @@
+package ruize.mindmateauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmateAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindmateAuthApplication.class, args);
+    }
+
+}
---
+++ MindMate_backend/mindmate-auth/src/test/java/ruize/mindmateauth/MindmateAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ruize.mindmateauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindmateAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
