cb86ab4d3534df099a4794140f9cd163b5f4a02e
iRuiZe <cnruize0503@gmail.com>
2025-08-18 09:17:04 +0800

    更新SQL内容

--- MindMate_backend/SQL/user.sql
+++ MindMate_backend/SQL/user.sql
@@ -1,8 +1,38 @@
-CREATE TABLE user
-(
-    phone_number           VARCHAR(20) PRIMARY KEY COMMENT '手机号，主键',
-    username               VARCHAR(50) COMMENT '用户名',
-    real_name              VARCHAR(50) COMMENT '真实姓名',
-    personalized_signature VARCHAR(255) COMMENT '个性签名',
-    email                  VARCHAR(100) COMMENT '电子邮箱'
-) COMMENT ='用户信息表';
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : 阿里云服务器
+ Source Server Type    : MySQL
+ Source Server Version : 80043 (8.0.43)
+ Source Host           : 47.107.85.154:3306
+ Source Schema         : mindmate
+
+ Target Server Type    : MySQL
+ Target Server Version : 80043 (8.0.43)
+ File Encoding         : 65001
+
+ Date: 18/08/2025 09:15:20
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+                         `phone_number` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '手机号，主键',
+                         `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',
+                         `real_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '真实姓名',
+                         `personalized_signature` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '个性签名',
+                         `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电子邮箱',
+                         PRIMARY KEY (`phone_number`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('15176951209', '琴声萧瑟月半弯', '邸睿泽', '琴声萧瑟月半弯，微风细抚花摇曳', 'cnruize0503@gmail.com');
+
+SET FOREIGN_KEY_CHECKS = 1;
